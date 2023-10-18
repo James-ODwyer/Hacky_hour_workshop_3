26d35685607cdb505d752212181c8fb6d8473177
Callum Le Lay <callum.lelay@csiro.au>
2023-10-18 14:32:53 +1100

    Modifications For Clarity And Adjust For Mystery
    Changes to sequence files
    Removed dirs 'References_mastadenovirusC5_strain5/' and 'References_mastadenovirusC5_strains/' and moved files to 'sequences/'. Files are renamed to:
        Human_mastadenovirus_C5_all -> human_adeno_5_genomes
        Human_mastadenovirus_C_any -> human_adeno_all_genomes
    Removed ',complete genome', and '|' from header names, and replaces spaces with underscores. This is so that full names are kept in alignment and tree building.
    
    Changes to script
    Changed variables in script to refer to new file names. Added a command to concatenate reference sequence and target sequence fasta files. Bit of name changing of variables and intermediate files, for clarity.
    
    Removed all intermediate and result data files, ready to rerun with new names and mystery virus included.

--- hacky_hour.sh
+++ hacky_hour.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-# Hacky Hour Workshop 4 - Script to align sequences, trim the alignment, and build a tree.
+# Hacky Hour Workshop 3
+# Hello! Welcome! The aim of the workshop, and this script, is to build a phylogenetic tree from nucleotide sequences on Petrichor, CSIRO's High Performance Computing Cluster (HPC). To do this we will be looking at this job submission script - usually job submission scripts will have a .sh (shell) suffix or .pbs. This script can be submitted to the job scheduler on the HPC, SLURM, to be run. Within this script we will provide SLURM with:
+#  1) what computing parameters we would like to use for the job, 
+#  2) what programs to load (allow us to use them in the job instance on the HPC),
+#  3) some variables to be used in later code
+#  4) run the tool MAFFT to align our sequences
+#  5) run trimal to trim the resulting multiple sequence alignment (MSA)
+#  6) run IQTree2 to build a phylogenetic tree from the trimmed MSA
 
-#SBATCH --job-name=hackhour4
+#SBATCH --job-name=hacky3
 #SBATCH --time=05:00:00
 #SBATCH --ntasks=12
 #SBATCH --nodes=1
@@ -16,33 +22,41 @@ module load iqtree/2.2.0.5
 # BASH variable used when running tools. It is best to specify file and directory loactions in variables
 # like this so that you can easily switch up the target and parameters in your commands.
 threads=12 # You can use the $SLURM_THREADS_PER_CORE if you want instead 
+name='human_adeno_5'
+refs='sequences/human_adeno_5_genomes.fa'
+target='sequences/mystery_virus.fa'
+outgroup='M73260.1_Mastadenovirus_h5_gene'
 
-indir='References_mastadenovirusC5_strain5'
-input='Human_mastadenovirus_C5_all.fasta' 
-
-outgroup='M73260.1'
-
+# Put reference sequences and our mystery virus in the same fasta file, ready for alignment.
+cat $refs $target > $name.fa
 
 # Perform multiple sequence alignment using MAFFT:
 # --nuc: Indicates that the input data is nucleotide sequences.
 # --auto: Automatically selects an appropriate algorithm.
 # --reorder: Reorders input sequences to optimize alignment.
 # --thread $threads: Specifies the number of threads for parallel processing.
-mafft --nuc --auto --reorder --thread $threads $indir/$input > alignment/${input}.aln
+mafft --nuc --auto --reorder --thread $threads $name.fa > alignment/$name.aln
 
 # Trim the alignment using trimAl:
-# -in ${input}.aln: Input alignment file.
-# -out ${input}.aln.trim: Output trimmed alignment file.
+# -in $name.aln: Input alignment file.
+# -out $name.trimaln: Output trimmed alignment file.
 # -gt 0.8: Remove columns with more than 80% gaps.
 # -st 0.001: Remove sequences with less than 0.1% similarity to others.
-trimal -in alignment/${input}.aln -out alignment/${input}.aln.trim -gt 0.8 -st 0.001
+trimal -in alignment/$name.aln -out alignment/$name.trimaln -gt 0.8 -st 0.001
 
 # Build a phylogenetic tree using IQ-TREE 2:
-# -s ${input}.aln.trim: Input alignment file.
-# --prefix ${input}.aln.trim.tree: Prefix for output tree file.
+# -s $name.trimaln: Input alignment file.
+# --prefix $name.tree: Prefix for output tree file.
 # -m TEST: Model selection (IQ-TREE will choose the best model).
 # --alrt 1000: Perform ultrafast bootstrap approximation with 1000 replicates.
 # -B 1000: Perform standard bootstrap analysis with 1000 replicates.
 # -T $threads: Number of threads for parallel computation.
 # -o $outgroup: Designate the specified taxon as the outgroup for tree rooting.
-iqtree2 -s alignment/${input}.aln.trim  --prefix tree/${input}.aln.trim.tree -m TEST --alrt 1000 -B 1000 -T $threads -o $outgroup
+iqtree2 -s alignment/$name.trimaln  --prefix tree/$name.tree -m TEST --alrt 1000 -B 1000 -T $threads -o $outgroup
+
+## Last, we can run this script on the HPC.
+# Log in by:
+#    iden@petrichor-login.hpc.csiro.au
+# Navigate to where you would like to run your script.
+# Run this script suing:
+#    sbatch hacky_hour.sh

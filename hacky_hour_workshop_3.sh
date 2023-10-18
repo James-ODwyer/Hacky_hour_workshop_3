5abcf5211510a4c5f67ce0f4f308304c5c31649d
lel005 <callum.lelay@csiro.au>
2023-10-18 14:52:40 +1100

    Run on HPC
    Ran script on HPC, found I had dropped the hashbang from script, so
    added that back. Also changed the the prefix name in the iqtree command
    to be neater.
    Added a dir called 'answers/' to hold slurm log file, but also pngs of
    tree figures.

--- hacky_hour_workshop_3.sh
+++ hacky_hour_workshop_3.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Hacky Hour Workshop 3
 # Hello! Welcome! The aim of the workshop, and this script, is to build a phylogenetic tree from nucleotide sequences on Petrichor, CSIRO's High Performance Computing Cluster (HPC). To do this we will be looking at this job submission script - usually job submission scripts will have a .sh (shell) suffix or .pbs. This script can be submitted to the job scheduler on the HPC, SLURM, to be run. Within this script we will provide SLURM with:
 #  1) what computing parameters we would like to use for the job, 
@@ -28,14 +29,14 @@ target='sequences/mystery_virus.fa'
 outgroup='M73260.1_Mastadenovirus_h5_gene'
 
 # Put reference sequences and our mystery virus in the same fasta file, ready for alignment.
-cat $refs $target > $name.fa
+cat $refs $target > sequences/$name.fa
 
 # Perform multiple sequence alignment using MAFFT:
 # --nuc: Indicates that the input data is nucleotide sequences.
 # --auto: Automatically selects an appropriate algorithm.
 # --reorder: Reorders input sequences to optimize alignment.
 # --thread $threads: Specifies the number of threads for parallel processing.
-mafft --nuc --auto --reorder --thread $threads $name.fa > alignment/$name.aln
+mafft --nuc --auto --reorder --thread $threads sequences/$name.fa > alignment/$name.aln
 
 # Trim the alignment using trimAl:
 # -in $name.aln: Input alignment file.
@@ -52,7 +53,7 @@ trimal -in alignment/$name.aln -out alignment/$name.trimaln -gt 0.8 -st 0.001
 # -B 1000: Perform standard bootstrap analysis with 1000 replicates.
 # -T $threads: Number of threads for parallel computation.
 # -o $outgroup: Designate the specified taxon as the outgroup for tree rooting.
-iqtree2 -s alignment/$name.trimaln  --prefix tree/$name.tree -m TEST --alrt 1000 -B 1000 -T $threads -o $outgroup
+iqtree2 -s alignment/$name.trimaln  --prefix tree/$name -m TEST --alrt 1000 -B 1000 -T $threads -o $outgroup
 
 ## Last, we can run this script on the HPC.
 # Log in by:
